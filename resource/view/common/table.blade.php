8e526db141795c0cc8e9427ec6169822117f2302
mwang7 <mwang7@i9i8.com>
2020-04-22 20:06:52 +0800

    优化

--- resource/view/common/table.blade.php
+++ resource/view/common/table.blade.php
@@ -12,7 +12,9 @@
                             <th>{{ $column['label'] ?? $column }}</th>
                             @endif
                         @endforeach
-                            <th>操作</th>
+                        @if (!empty($action))
+                            <th style="width: 60px;">操作</th>
+                        @endif
                         </tr>
                     </thead>
                     <tbody>
@@ -25,10 +27,21 @@
                             <td>{{ $item->{$column['attribute'] ?? $column} ?? '' }}</td>
                             @endif
                         @endforeach
+                        @if (!empty($action))
                             <td>
-                                <a href="/admin/message/{{ $item['id'] }}/edit"><i class="fas fa-edit"></i></a>
-                                <a href="javascript:void(0);" data-id="{{ $item['id'] }}" class="tree_branch_delete"><i class="fas fa-trash"></i></a>
+                                @foreach($action as $op)
+                                    @if ($op == 'view')
+                                    <a href="/{{ $_path }}/{{ $item['id'] }}"><i class="fas fa-eye"></i></a>&nbsp;&nbsp;
+                                    @elseif ($op == 'edit')
+                                    <a href="/{{ $_path }}/{{ $item['id'] }}/edit"><i class="fas fa-edit"></i></a>&nbsp;&nbsp;
+                                    @elseif ($op == 'delete')
+                                    <a href="javascript:void(0);" data-id="{{ $item['id'] }}" class="grid-row-delete"><i class="fas fa-trash"></i></a>&nbsp;&nbsp;
+                                    @else
+                                    {!! $op($item) !!}
+                                    @endif
+                                @endforeach
                             </td>
+                        @endif
                         </tr>
                     @endforeach
                     </tbody>
